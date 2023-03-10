e2a51ea8a04c139e7dc5ab0df9596757c5705d6a
marie <marie_maunoury@hotmail.fr>
2023-03-10 11:48:21 +0100

    Add post url /api/shipping

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-// Add Express
 const express = require("express");
-
-// Initialize Express
 const app = express();
+app.use(express.json());
+// Declare orders as a global variable
+const orders = [];
 
 // Create GET request
 app.get("/api/ping", (req, res) => {
   res.send("PONG");
+});
+
+// Create POST request for /api/shipping
+app.post("/api/shipping", (req, res) => {
+  // Send a response to the client + code status
+  // Get the order ID and number of products from the request body
+  const { orderId, nbProducts } = req.body;
+  // Save the order details to memory or database
+  // For example, you can store it in an array
+  orders.push({ orderId, nbProducts });
+  res.status(204).send("Order details saved successfully.");
 });
 
 // Initialize server
